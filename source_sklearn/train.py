9925b0894b9bbb406e60877d2ee58ff73ac7b114
prateeksawhney97 <prateek.sawhney97@gmail.com>
2020-05-23 14:24:03 +0530

    deployed using sagemaker

--- source_sklearn/train.py
+++ source_sklearn/train.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
-
+from sklearn.svm import LinearSVC
 
 # Provided model load function
 def model_fn(model_dir):
@@ -56,11 +56,11 @@ if __name__ == '__main__':
     
 
     ## TODO: Define a model 
-    model = None
+    model = LinearSVC()
     
     
     ## TODO: Train the model
-    
+    model.fit(train_x, train_y)
     
     
     ## --- End of your code  --- ##
